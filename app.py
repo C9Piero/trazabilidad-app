ecfd31845356b4cb0ab5222133e5a55ceb642555
Piero <90986839+C9Piero@users.noreply.github.com>
2026-08-17 15:55:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1448,18 +1448,18 @@ else:
             )
 
             # CÁLCULO DINÁMICO REAJUSTADO A TIEMPOS REALES DE UPCYCLING
-                if peso_total_recibido <= 10:
-                    dias_calc_corte = 1
-                    hdia_calc_corte = 3.0  # 3 hrs totales por persona
-                elif peso_total_recibido <= 30:
-                    dias_calc_corte = 1
-                    hdia_calc_corte = 6.0  # 6 hrs totales por persona
-                elif peso_total_recibido <= 50:
-                    dias_calc_corte = 2
-                    hdia_calc_corte = 6.0  # 12 hrs totales por persona
-                else:
-                    dias_calc_corte = max(2, int(peso_total_recibido / 25))
-                    hdia_calc_corte = 8.0
+            if peso_total_recibido <= 10:
+                dias_calc_corte = 1
+                hdia_calc_corte = 3.0  # 3 hrs totales por persona
+            elif peso_total_recibido <= 30:
+                dias_calc_corte = 1
+                hdia_calc_corte = 6.0  # 6 hrs totales por persona
+            elif peso_total_recibido <= 50:
+                dias_calc_corte = 2
+                hdia_calc_corte = 6.0  # 12 hrs totales por persona
+            else:
+                dias_calc_corte = max(2, int(peso_total_recibido / 25))
+                hdia_calc_corte = 8.0
 
             st.markdown("#### Operaciones – Corte y Logística")
 
