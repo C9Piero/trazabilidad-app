539eece8dd69e4f32a47cd37af0c16845d457833
Piero <90986839+C9Piero@users.noreply.github.com>
2026-08-24 15:49:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2450,11 +2450,11 @@ else:
                 col_gen1, col_gen2 = st.columns([2, 1])
 
                 if col_gen2.button("💾 Guardar como Borrador", use_container_width=True):
-                try:
-                    with st.spinner("Guardando en la base de datos y subiendo fotos a la nube..."):
-                        datos_detalle = procesar_fotos_y_armar_detalle()
+                    try:
+                        with st.spinner("Guardando en la base de datos y subiendo fotos a la nube..."):
+                            datos_detalle = procesar_fotos_y_armar_detalle()
 
-                        datos_borrador = {
+                            datos_borrador = {
                             "codigo": codigo_proy,
                             "cliente": cliente,
                             "ruc": ruc,
