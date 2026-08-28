0087060756eb385bb6db71c9b02f938b0bd79105
Piero <90986839+C9Piero@users.noreply.github.com>
2026-08-28 18:01:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2901,7 +2901,7 @@ else:
     
                 c4, c7, c8, c9 = st.columns(4)
                 
-                opciones_tipo_proyecto = ["Upcycling", "Residuos / Mermas", "Cambio de logo", "Banner", "Mixto"]
+                opciones_tipo_proyecto = ["Upcycling", "Mermas Textiles", "Cambio logo", "Banner"]
                 tipo_actual = p_edit.get("tipo_proyecto", "Upcycling")
                 idx_tipo = opciones_tipo_proyecto.index(tipo_actual) if tipo_actual in opciones_tipo_proyecto else 0
     
